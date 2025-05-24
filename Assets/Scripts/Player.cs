9ad9b39c53b00763fcebc27696925d3c463c622a
Eusébio Alves <eusebio-alves@protonmail.com>
2025-05-24 13:29:44 +0100

    Play sound while pressing

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,16 +7,20 @@ public class Player : MonoBehaviour
     private bool _playerButtonDown = false;
     private float _buttonDownTime = 0.0f;
 
+    [SerializeField] private AudioSource _audioSource;
+
     private void Update()
     {
         if (Input.GetButtonDown("Jump"))
         {
             _playerButtonDown = true;
+            _audioSource.Play();
         }
 
         if (Input.GetButtonUp("Jump"))
         {
             _playerButtonDown = false;
+            _audioSource.Stop();
 
             if (_buttonDownTime <= 0.2f)
             {
