f9454d4f2bb0674e819c1af4e838592719dfe352
Eusébio Alves <eusebio-alves@protonmail.com>
2025-05-24 15:40:11 +0100

    eyes

---
+++ Assets/Scripts/CameraPerspective.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class CameraPerspective : MonoBehaviour
+{
+    public Transform cameraHolder;    // Yaw (horizontal)
+    public Transform cameraTransform; // Pitch (vertical)
+    public float mouseSensitivity = 100f;
+
+    float xRotation = 0f;
+    float yRotation = 0f;
+
+    void Start()
+    {
+        cameraHolder = transform;
+        cameraTransform = Camera.main.transform; 
+        Cursor.lockState = CursorLockMode.Locked; // Hide & lock mouse
+
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        // --- Pitch (up/down)
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        cameraTransform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        // --- Yaw (left/right)
+        yRotation += mouseX;
+        yRotation = Mathf.Clamp(yRotation, -45f, 45f); // Limit to ±45 degrees
+        cameraHolder.localRotation = Quaternion.Euler(0f, yRotation, 0f);
+    }
+}
