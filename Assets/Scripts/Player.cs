35819858aa1ec3762536bbc69e986f389e830e3b
Eusébio Alves <eusebio-alves@protonmail.com>
2025-05-25 07:31:27 +0100

    Only allow to send morse while looking at telegraph

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,34 +8,58 @@ public class Player : MonoBehaviour
     private float _buttonDownTime = 0.0f;
 
     [SerializeField] private AudioSource _audioSource;
-    [SerializeField] private Transform _telegraphTransform;
+    [SerializeField] private Transform _telegraphButton;
+    [SerializeField] private MeshRenderer _telegraphRenderer;
 
     private void Update()
     {
-        if (Input.GetButtonDown("Jump"))
+        if (_telegraphRenderer.isVisible)
         {
-            _playerButtonDown = true;
-            _audioSource.Play();
-            _telegraphTransform.localRotation = Quaternion.Euler(-67.9f, 0, 0); // Rotate telegraph to indicate button press
-        }
-
-        if (Input.GetButtonUp("Jump"))
-        {
-            _playerButtonDown = false;
-            _audioSource.Stop();
-            _telegraphTransform.localRotation = Quaternion.Euler(-89.98f, 0, 0);
-
-            if (_buttonDownTime <= 0.2f)
+            if (Input.GetButtonDown("Jump"))
             {
-                _game.CompareInputToCurrentMorse('.');
-            }
-            else
-            {
-                _game.CompareInputToCurrentMorse('-');
+                _playerButtonDown = true;
+                _audioSource.Play();
+                _telegraphButton.localRotation = Quaternion.Euler(-67.9f, 0, 0); // Rotate telegraph to indicate button press
             }
 
-            _buttonDownTime = 0.0f;
+            if (Input.GetButtonUp("Jump"))
+            {
+                _playerButtonDown = false;
+                _audioSource.Stop();
+                _telegraphButton.localRotation = Quaternion.Euler(-89.98f, 0, 0);
+
+                if (_buttonDownTime <= 0.2f)
+                {
+                    _game.CompareInputToCurrentMorse('.');
+                }
+                else
+                {
+                    _game.CompareInputToCurrentMorse('-');
+                }
+
+                _buttonDownTime = 0.0f;
+            }
         }
+        else
+        { 
+            if (_playerButtonDown == true)
+            { 
+                _playerButtonDown = false; // Reset button state if telegraph is not visible
+                _audioSource.Stop();
+                _telegraphButton.localRotation = Quaternion.Euler(-89.98f, 0, 0);
+                if (_buttonDownTime <= 0.2f)
+                {
+                    _game.CompareInputToCurrentMorse('.');
+                }
+                else
+                {
+                    _game.CompareInputToCurrentMorse('-');
+                }
+
+                _buttonDownTime = 0.0f;
+            }
+        }
+
 
         if (_playerButtonDown)
         {
